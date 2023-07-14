1b79ea119c4ba3b84d8e6c22376fa03d8e6aaae4
pashaTuna <138866526+pashaTuna@users.noreply.github.com>
2023-07-14 16:18:04 +0300

    Add files via upload

---
+++ bunny-jump-first-look/src/lib/phaser.js
@@ -0,0 +1,3 @@
+export default window.Phaser
+
+/* This is main */
---
+++ bunny-jump-first-look/src/main.js
@@ -0,0 +1,11 @@
+import Phaser from "./lib/phaser.js"
+
+console.dir(Phaser)
+
+/* This is main */
+
+export default new Phaser . Game ({
+  type: Phaser.AUTO,
+  width: 480,
+  height:640  
+})
